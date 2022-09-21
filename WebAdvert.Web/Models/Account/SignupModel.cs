bd3a39572f9618de61030de5e8a456de57391cf8
Matt <joeboxers28@yahoo.com>
2022-09-20 22:03:55 -0400

    end of section 14

---
+++ WebAdvert.Web/Models/Account/SignupModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Account
+{
+    public class SignupModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(40, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password Confirmation is required.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and its confirmation do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
